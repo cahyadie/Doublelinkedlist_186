1d40f14f799e176dc505e720b3f40b364d14eead
cahyadie <cahyadiilham12@gmail.com>
2023-05-25 19:54:47 +0700

    make void revtraverse

--- Doublelinkedlist_186/Doublelinkedlist_186.cpp
+++ Doublelinkedlist_186/Doublelinkedlist_186.cpp
@@ -116,6 +116,23 @@ void DoubleLinkedList::traverse() {
 	}
 }
 
+void DoubleLinkedList::revtraverse() {
+	if (listEmpty()) {
+		cout << "\nList is emptty" << endl;
+	}
+	
+	else {
+		cout << "\nRecord in describing order of roll number are : " << endl;
+		Node* currentNode = START;
+		while (currentNode->next != NULL)
+			currentNode = currentNode->next;
+
+		while (currentNode != NULL) {
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->prev;
+		}
+	}
+}
 
 
 
