6d9fb246adcd5504f6051c77de3034ed8daacde6
cahyadie <cahyadiilham12@gmail.com>
2023-05-25 20:07:16 +0700

    void searchData

--- Doublelinkedlist_186/Doublelinkedlist_186.cpp
+++ Doublelinkedlist_186/Doublelinkedlist_186.cpp
@@ -148,4 +148,20 @@ void DoubleLinkedList::hapus() {
 		cout << "\nrecord with roll number " << rollNo << "deleted " << endl;
 }
 
-
+void DoubleLinkedList::SearchData() {
+	if (listEmpty() == true) {
+		cout << "\nlist is empty" << endl;
+	}
+	Node* prev, * curr;
+	prev = curr = NULL;
+	cout << "nEnter the roll number of the student whose record you want to search : ";
+	int num;
+	cin >> num;
+	if (DoubleLinkedList::search(num, &prev, &curr) == false)
+		cout << "\nRecord not found" << endl;
+	else {
+		cout << "\nRecord found" << endl;
+		cout << "\nRoll number : " << curr->noMhs << endl;
+		cout << "\nName : " << curr->name << endl;
+	}
+}
