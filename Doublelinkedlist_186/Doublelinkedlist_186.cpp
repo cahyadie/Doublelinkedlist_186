a48527083475712be1f14c482881c9b7df8bdcc5
cahyadie <cahyadiilham12@gmail.com>
2023-05-25 15:51:07 +0700

    insert a node in the begining of doulby

--- Doublelinkedlist_186/Doublelinkedlist_186.cpp
+++ Doublelinkedlist_186/Doublelinkedlist_186.cpp
@@ -29,3 +29,29 @@ DoubleLinkedList::DoubleLinkedList() {
 	START = NULL;
 }
 
+void DoubleLinkedList::addNode() {
+	int nim;
+	string nm;
+	cout << "\nEnter the roll number of the student : ";
+	cin >> nim;
+	cout << "\nEnter the name of the student : ";
+	cin >> nm;
+	Node* newNode = new Node();
+	newNode->noMhs = nim;
+	newNode->name = nm;
+
+	/*insert a node in the beginning of a doulby - linked list*/
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs) {
+			cout << "\nDuplicate number not allowed" << endl;
+			return;
+		}
+		newNode->next = START;
+		if (START != NULL)
+			START->prev = newNode;
+		newNode->prev = NULL;
+		START = newNode;
+		return;
+	}
+}
+
